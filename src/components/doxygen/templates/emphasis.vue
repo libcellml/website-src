db833e2cc70ff3d800cb0e7dc5b8f68bcdecdf42
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-02 16:16:00 +1200

    Add emphasis to doxygen templates.

---
+++ src/components/doxygen/templates/emphasis.vue
@@ -0,0 +1,21 @@
+<script>
+import { doxygenChildren } from '@/mixins/DoxygenChildren'
+
+export default {
+  name: 'emphasis',
+  mixins: [doxygenChildren],
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      tagName: 'em',
+      element: this.data.element,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
