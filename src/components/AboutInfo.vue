e82e0f75db2d65aa241bb10d93131f8a66f3091f
Keri Moyle <k.moyle@auckland.ac.nz>
2020-12-10 11:29:22 +1300

    Footer link and about page working

--- src/components/AboutInfo.vue
+++ src/components/AboutInfo.vue
@@ -4,12 +4,11 @@
       <v-col>
         <h1>About libCellML</h1>
 
-        <p>
+        <h3>
           libCellML is an easy-to-use library aiming to be useful to developers of CellML applications.
-        </p>
+        </h3>
         <p>
           The purpose of CellML is to store and exchange computer-based mathematical models, allowing scientists to share models even if they are using different model-building software.
-
           The purpose of libCellML is to create, manipulate, serialise, deserialise, validate and instantiate CellML models.  Alongside that, libCellML offers functionality for annotation support, model debugging, mathematical analysis and code generation.
         </p>
 
@@ -25,8 +24,9 @@
           A complete list of the developers and contributors to the libCellML project are given on the <a href="https://github.com/cellml/libcellml/graphs/contributors">libCellML Github repository page</a>.
         </p>
        
+        <h3>Supporters</h3>
+        The libCellML team acknowledges the generous support of our sponsors. 
 
-        <h3>libCellML is supported by</h3>
         <v-container class="image-box">
           <v-row>
             <v-col class="col-12 col-sm-6 col-md-4">
