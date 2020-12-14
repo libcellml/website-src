8990dda2ea93e59002353a4a16ca8e012e61026d
Keri Moyle <k.moyle@auckland.ac.nz>
2020-12-14 13:06:03 +1300

    Added back button to 404 page and echoes broken link

---
+++ src/components/BrokenLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p>The page that you've requested:</p>
+        <h4>{{ lastURL }}</h4>
+        <br/>
+        <p>
+          does not exist.
+          Please use the "back" button on your browser to return to where you were, or start again using the 
+          links in the menu bar.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'BrokenLink',
+
+  computed: {
+    lastURL() {
+      return this.$store.getters.getLastURL
+    }
+  },
+}
+</script>
+
+<style scoped></style>
