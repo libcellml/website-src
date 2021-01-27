ee28f3c04e2f7c079f545f3f72273c54e7d94031
Keri Moyle <k.moyle@auckland.ac.nz>
2021-01-28 11:15:59 +1300

    general button, changed docs to archive

---
+++ src/components/DocumentationArchive.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="col-12 col-md-4">
+        <v-btn block :class="'big-button'" to="/documentation/api">
+          <v-icon color="white" x-large>mdi-book-open-page-variant</v-icon>
+          <br />
+          API Documentation
+        </v-btn>
+        <div>
+          <br />
+          <ul>
+            <li
+              v-for="(version, index) in apiVersions"
+              :key="'api_reference_' + index"
+            >
+              <router-link :to="{ path: `documentation/api/${version}` }" >
+                API Documentation {{ version }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </v-col>
+
+      <v-col class="col-12 col-md-4">
+        <v-btn block :class="'big-button'" to="/documentation/guides">
+          <v-icon color="white" x-large>mdi-account-group</v-icon>
+          <br />
+          Users' Guides
+        </v-btn>
+        <div>
+          <br />
+          <ul>
+            <li
+              v-for="(version, index) in tutorialVersions"
+              :key="'guide_' + index"
+            >
+              <router-link :to="{ path: `documentation/guides/${version}` }">
+                Users' Guides {{ version }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </v-col>
+
+      <v-col class="col-12 col-md-4">
+        <v-btn block :class="'big-button'">
+          <v-icon color="white" x-large>mdi-cogs</v-icon>
+          <br />
+          Developers' Guides
+        </v-btn>
+        <div><br />TODO Stuff goes here</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { getDoxygenVersions, getSphinxVersions } from '@/js/versions'
+
+export default {
+  name: 'DocumentationArchive',
+
+  data: () => {
+    return { apiVersions: [], tutorialVersions: [] }
+  },
+  created() {
+    this.apiVersions = getDoxygenVersions()
+    this.tutorialVersions = getSphinxVersions()
+  },
+}
+</script>
+
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
