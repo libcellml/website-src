8644b93b34649b74e4699e2e14fccbda88f218fb
hsorby <h.sorby@auckland.ac.nz>
2021-01-27 14:57:35 +1300

    Add some icons for downloading and documentation.

---
+++ src/icons/MacOSOSIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg id="macOS" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 136.69 177.3">
+    <title>macos-os-icon</title>
+    <path
+      d="M330.46,161.21c-17.89,13.69-14.26,35.67,3.3,45.53-3.52,11.12-8.65,21.45-18.83,27.95-3.13,2-8.61,2.43-12.2,1.21-9.76-3.32-19.08-4-28.62.15-6.1,2.68-11.35.59-15.59-3.74-15.35-15.69-22.68-34.32-20-56.45,2-16.71,20.64-31.46,38.39-23.58,7.35,3.26,13.89,2,21-.48C311.44,147.1,324.05,151,330.46,161.21Z"
+      transform="translate(-217.52 -122.61)"
+    />
+    <path
+      d="M309.61,122.61c.81,14.39-10.19,27-22.45,26.39C285.27,136.1,294.65,124.94,309.61,122.61Z"
+      transform="translate(-217.52 -122.61)"
+    />
+    <text class="cls-1" transform="translate(0 167.19)">macOS</text>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'UserGuides',
+}
+</script>
+
+<style scoped>
+.cls-1 {
+  font-size: 35.8px;
+  fill: #3a3a39;
+  font-family: OpenSans-Semibold, Open Sans;
+  font-weight: 600;
+  letter-spacing: 0.09em;
+}
+svg {
+  height: fit-content;
+  width: fit-content;
+}
+</style>
---
+++ src/icons/WebAssemblyIcon.vue
@@ -0,0 +1,64 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 213.61 209.34">
+    <title>webassembly</title>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Webassembly">
+        <text class="cls-1" transform="translate(64.84 156.68)">
+          <tspan class="cls-2">
+            WE
+            <tspan class="cls-3" x="60.29" y="0">B</tspan>
+          </tspan>
+          <tspan class="cls-4">
+            <tspan class="cls-3" x="-64.84" y="42.96">ASSEMBL</tspan>
+            <tspan x="128.71" y="42.96">Y</tspan>
+          </tspan>
+        </text>
+        <g id="icon">
+          <path
+            class="cls-5"
+            d="M119.12,0c0,.19,0,.38,0,.58a12.35,12.35,0,0,1-24.69,0c0-.2,0-.39,0-.58H53V107.62H160.61V0ZM104.37,96.1,99.13,70.17H99L93.39,96.1H86.17L78,58h7.13L90,83.93h.1L95.93,58h6.68l5.28,26.25H108L113.55,58h7l-9.09,38.1Zm39.27,0-2.43-8.48H128.39l-1.87,8.48h-7.17L128.58,58h11.25L151,96.1Z"
+          />
+          <polygon
+            class="cls-5"
+            points="132.87 67.39 129.76 81.37 139.44 81.37 135.86 67.39 132.87 67.39"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'UserGuides',
+}
+</script>
+
+<style scoped>
+.cls-1 {
+  font-size: 35.8px;
+  fill: #3a3a39;
+  font-family: OpenSans-Semibold, Open Sans;
+  font-weight: 600;
+}
+.cls-2 {
+  letter-spacing: 0.09em;
+}
+.cls-3,
+.cls-4 {
+  letter-spacing: 0.15em;
+}
+.cls-4,
+.cls-5 {
+  font-family: OpenSans, Open Sans;
+  font-weight: 400;
+}
+.cls-5 {
+  fill: #605eaa;
+}
+
+svg {
+  height: fit-content;
+  width: fit-content;
+}
+</style>
