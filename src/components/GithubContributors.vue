6cc7b76184e11306f0745579f044554f818ab4b2
Keri Moyle <k.moyle@auckland.ac.nz>
2021-02-24 21:04:11 +1300

    Working, but needs authentication to avoid max 60 calls/hour limit

--- src/components/GithubContributors.vue
+++ src/components/GithubContributors.vue
@@ -1,60 +1,41 @@
 <template>
   <v-container>
     <!-- Option 3: Names in combined list -->
-    <ul v-for="person in allContributors" :key="`person-${person.user}`">
-      <li>{{ person.user_data.name }}</li>
+    <!-- <ul v-for="person in allContributors" :key="`person-${person.login}`">
+      <li>{{ person }}</li>
       <li>
-        <!-- {{ person.user_data.avatar_url }} -->
-        <img :src="person.user_data.avatar_url" class="avatar" />
+        {{ person.user_data }}
+         <img :src="person.user_data.avatar_url" class="avatar" /> 
       </li>
-    </ul>
-
-    <!-- OPTION 2: Names in separate lists -->
-    <!-- <v-row>
-      <v-col class="col-12 col-md-4">
-        <h4>libCellML contributors</h4>
-        <ul
-          v-for="person in libcellmlContributors"
-          :key="`person-id-${person.id}`"
-        >
-          <li>{{ person.login }} ({{ person.contributions }})</li>
-        </ul>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <h4>Website contributors</h4>
-        <ul
-          v-for="person in websiteContributors"
-          :key="`person-id-${person.id}`"
-        >
-          <li>{{ person.login }} ({{ person.contributions }})</li>
-        </ul>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <h4>Tutorial contributors</h4>
-        <ul
-          v-for="person in tutorialsContributors"
-          :key="`person-id-${person.id}`"
-        >
-          <li>{{ person.login }} ({{ person.contributions }})</li>
-        </ul>
-      </v-col>
-    </v-row> -->
+    </ul> -->
 
     <!-- OPTION 1: Avatars -->
-    <!-- <v-row>
+    <v-row>
       <v-col
         v-for="person in allContributors"
-        :key="`person-id-${person.user}`"
+        :key="`person-id-${person.login}`"
         class="col-1"
-      > -->
-    <!-- <v-avatar> -->
-    <!-- <img :src="person.data.user_data.avatar_url" class="avatar" /> -->
-    <!-- </v-avatar> -->
-    <!-- {{person.data.user_data}}
+      >
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <a v-on="on" :href="person.url" target="_blank">
+              <v-avatar>
+                <img :src="person.avatar_url" :alt="person.name" />
+              </v-avatar>
+            </a>
+          </template>
+          <span>
+            <strong>{{ person.name }}</strong> contributes to:
+            <ul
+              v-for="repo in person.repos"
+              :key="`person-${person.login}_${repo}`"
+            >
+              <li>{{ repo }}</li>
+            </ul>
+          </span>
+        </v-tooltip>
       </v-col>
-    </v-row> -->
+    </v-row>
   </v-container>
 </template>
 
@@ -75,152 +56,119 @@ export default {
   name: 'GithubContributors',
   data: () => ({
     repo_data: {},
-    // contributors: {},
+    user_data: {},
   }),
   mounted: function () {
-    // setTimeout(() => {
-    repos.forEach((r) => {
-      this.GitHubAPI.get(
-        '/repos/' + r.org + '/' + r.repo + '/contributors',
-        {},
-        [this.repo_data, r.org + '_' + r.repo],
-      )
+    function githubGetContributors(org, repo) {
+      let p = new Promise(function (pResolve, pReject) {
+        let req = new XMLHttpRequest()
+        let url =
+          'https://api.github.com/repos/' + org + '/' + repo + '/contributors'
+        req.open('GET', url)
+        req.onload = function () {
+          if (req.status == 200) {
+            pResolve(req.response)
+          } else {
+            pReject("Oops, can't access repository: " + org + '/' + repo)
+          }
+        }
+        req.send()
+      })
+      return p
+    }
+
+    function githubGetUser(user) {
+      let p = new Promise(function (pResolve, pReject) {
+        let req = new XMLHttpRequest()
+        let url = 'https://api.github.com/users/' + user.login
+        req.open('GET', url)
+        req.onload = function () {
+          if (req.status == 200) {
+            pResolve(req.response)
+          } else {
+            pReject("Oops, can't get user: " + user.login)
+          }
+        }
+        req.send()
+      })
+      return p
+    }
+
+    let fetchContributors = []
+    for (let r in repos) {
+      fetchContributors.push(githubGetContributors(repos[r].org, repos[r].repo))
+    }
+
+    Promise.all(fetchContributors).then((values) => {
+      let users = {}
+      for (var v in values) {
+        let temp = JSON.parse(values[v])
+        for (let u in temp) {
+          let user = temp[u].login
+          if (user.includes(skipUser)) {
+            console.log("Skipping ", user)
+            continue
+          } else if (users[user]) {
+            users[user].repos.push(repos[v])
+          } else {
+            users[user] = { login: user, repos: [repos[v],] }
+          }
+        }
+      }
+      console.log("users: ",users)
+
+      // let users = []
+      // for (var v in values) {
+      //   let temp = JSON.parse(values[v])
+      //   for (let u in temp) {
+      //     users.push(temp[u].login)
+      //   }
+      // }
+      // let uniqueUsers = [...new Set(users)]
+      // function notBot(user) {
+      //   return !user.includes(skipUser)
+      // }
+      // uniqueUsers = uniqueUsers.filter(notBot)
+
+      let fetchUsers = []
+      for (let u in users) {
+        fetchUsers.push(githubGetUser(users[u]))
+      }
+
+      Promise.all(fetchUsers).then((userDataArray) => {
+        let userData = []
+        for (let u in userDataArray) {
+          let tempUser = JSON.parse(userDataArray[u])
+          userData.push({
+            name: tempUser.name,
+            login: tempUser.login,
+            avatar_url: tempUser.avatar_url,
+            url: tempUser.html_url,
+          })
+        }
+        this.user_data = userData
+      })
     })
-
-    //   const data = { ...this.repo_data }
-
-    //   let users = []
-
-    //   repos.forEach((r) => {
-    //     let key = r.org + '_' + r.repo
-    //     if (data[key] !== undefined) {
-    //       data[key].forEach((u) => {
-    //         if (!u.login.includes(skipUser)) {
-    //           users.push(u.login)
-    //         }
-    //       })
-    //     }
-    //   })
-
-    //   let uniqueUsers = new Set(users)
-
-    //   uniqueUsers.forEach((u) => {
-    //     let person = {}
-    //     person.user = u
-    //     person.repos = []
-
-    //     this.GitHubAPI.get('/users/' + u, {}, [person, 'user_data'])
-
-    //     console.log('person = ')
-    //     console.log(person)
-    //     this.contributors.push(person)
-    //   })
-
-    //   console.log(this.contributors)
-    // }, this.$store.getters.getTransitionDelay)
   },
 
   computed: {
     allContributors() {
-      // return this.contributors
-      const data = { ...this.repo_data }
-
-      let users = []
-      if (Object.keys(data).length == repos.length) {
-        repos.forEach((r) => {
-          let key = r.org + '_' + r.repo
-          data[key].forEach((u) => {
-            if (!u.login.includes(skipUser)) {
-              users.push(u.login)
-            }
-          })
-        })
-
-        let uniqueUsers = new Set(users)
-        let userInfo = []
-
-        uniqueUsers.forEach((u) => {
-          let person = {}
-          person.user = u
-          person.repos = []
-
-          // let temp = {}
-          this.GitHubAPI.get('/users/' + u, {}, [person, 'user_data'])
-
-          //     // const userData = { ...temp.user_data }
-          //     // const userData = JSON.parse(JSON.stringify( temp.user_data ))
-          //     // const userData = Object.assign({}, temp)
-          //     // person.data = userData
-          //     // console.log("temp = ")
-          //     // console.log(temp)
-          //     // console.log("userData = ")
-          //     // console.log(userData)
-          //     // person.data = userData
-
-          repos.forEach((r) => {
-            const repoKey = r.org + '_' + r.repo
-            const userInRepo = data[repoKey].filter(function (el) {
-              return el.login === u
-            })
-            if (userInRepo.length > 0) {
-              person.repos.push(r.name)
-            }
-          })
-          console.log('person = ')
-          console.log(person)
-          userInfo.push(person)
-        })
-
-        return userInfo
-      }
-
-      return []
+      return this.user_data
     },
-    // allContributors() {
-    //   // const c1 = this.contributors.libcellml
-    //   // const c2 = this.contributors.website_src
-    //   // const c3 = this.contributors.libcellml_tutorials
-    //   // console.log(c1)
-    //   // console.log(c2)
-    //   // console.log(c3)
-
-    //   // let users = c1 + c2 + c3
-    //   // .map((a) => a.login)
-    //   alert(users)
-    //   console.log(users)
-    //   let userInfo = []
-    //   let info = {}
-
-    //   // combining info for duplicated user names
-    //   users.forEach((user) => {
-    //     this.GitHubAPI.get('users/' + user, {}, [info, 'names'])
-    //     userInfo.push({
-    //       contributions:
-    //         c1[user].contributions +
-    //         c2[user].contributions +
-    //         c3[user].contributions,
-    //       name: info.name,
-    //       login: user,
-    //     })
-    //   })
-
-    //   // objs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0))
-    //   // Make a list by combining all repositories, then re-order the dict according to the totals.
-    //   return userInfo
-    // },
   },
 }
 </script>
 
 <style scoped>
 .avatar {
-  width: 5rem;
+  max-width: 4rem;
   border-radius: 50%;
 }
-ul {
-  list-style-type: none;
+.avatar_name {
+  /* max-width: 4rem; */
+  text-align: center;
 }
-li::before {
-  content: '- ';
+.avatar_card {
+  padding: 0.5rem;
 }
 </style>
