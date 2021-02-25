9181604d4524325219a4a94e84b9fe49e09867e3
Keri Moyle <k.moyle@auckland.ac.nz>
2021-02-25 16:03:35 +1300

    link to main about heading from acknowledgements

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@
         </v-col>
         <v-col class="text-right">
           <p id="footer-ack">
-            <router-link to="/#acknowledgements">Acknowledgements</router-link>
+            <router-link to="/#about">Acknowledgements</router-link>
           </p>
         </v-col>
       </v-row>
