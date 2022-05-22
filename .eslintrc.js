124d4aea66249ab50d4ec0f5c129b03bdfcffbb2
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-05-22 20:13:29 +1200

    Update packages and lint code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "plugin:prettier/recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
+  ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
+import Vue from 'vue'
 
 import * as notifications from '@/store/modules/notifications.js'
 
