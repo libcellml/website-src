e58b8755709b0ea3ea898590d9cf2ad03234949f
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-06-01 21:35:29 +1200

    Set build unminified for debugging.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false
+  },
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
