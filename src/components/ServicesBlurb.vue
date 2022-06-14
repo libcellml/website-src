bd3ccd147d52ebb4d0cd061716bac5576d107566
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-06-14 21:59:44 +1200

    Fix up link colouring on home page.

--- src/components/ServicesBlurb.vue
+++ src/components/ServicesBlurb.vue
@@ -1,4 +1,4 @@
 <template>
   <h3>Further information</h3>
-  <span>For further information on these services please visit the <router-link to="/services">Services page</router-link>.</span>
+  <p>For further information on these services please visit the <router-link to="/services">Services page</router-link>.</p>
 </template>
