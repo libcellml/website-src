95ad261cf2d8baa6dc2cd109ccdc16aeacd13506
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-06-15 14:24:26 +1200

    Update github-deploy.mjs
    Try going back to ssh authentication.

--- .github/scripts/github-deploy.mjs
+++ .github/scripts/github-deploy.mjs
@@ -16,16 +16,13 @@ import * as fs from 'fs'
       console.log('Production run.')
     }
 
-    console.log("--------")
-    console.log(process.env)
-    console.log(process.env.BOT_ACCESS_TOKEN)
-    const deployRepo = `https://libcellml-bot:${process.env.BOT_ACCESS_TOKEN}@github.com/libcellml/${cname}.git`
+    const deployRepo = `git@github.com:libcellml/${cname}.git`
     const result = await execa('git', ['branch', '--show-current'])
     const currentGitBranch = result.stdout
 
     const workingGitBranch = 'dist'
     await execa('git', ['checkout', '--orphan', workingGitBranch])
-    // await execa('git', ['remote', 'add', 'deploy', deployRepo])
+    await execa('git', ['remote', 'add', 'deploy', deployRepo])
     console.log('Building ...')
     await execa('yarn', ['run', 'build'])
     // Understand if it's dist or build folder
@@ -47,7 +44,7 @@ import * as fs from 'fs'
     ])
     console.log('Building ... success.')
     console.log(`Pushing to deploy/${targetGitBranch} ...`)
-    await execa('git', ['push', deployRepo, `HEAD:${targetGitBranch}`, '--force'])
+    await execa('git', ['push', 'deploy', `HEAD:${targetGitBranch}`, '--force'])
     console.log(`Pushing to deploy/${targetGitBranch} ... success.`)
     console.log('Cleaning up ...')
     await execa('rm', ['-r', folderName])
