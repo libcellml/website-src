d3ffacf114e8ce9aae6ef2aaceac5d1e6f27e846
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-07-13 21:15:08 +1200

    Add some information on the validation provenance of the user documentation.

--- src/views/DocumentationUserHome.vue
+++ src/views/DocumentationUserHome.vue
@@ -145,15 +145,77 @@
       </li>
     </ul>
   </div>
+  <v-row>
+    <v-col>
+      <h1>Validation Provenance</h1>
+      <p v-if="validationProvenanceIsKnown">
+        This documentation was validated with
+        <a :href="framework_url" target="_blank"> {{ framework_ref }}</a>
+        of the testing framework and
+        <a :href="database_url" target="_blank"> {{ database_ref }}</a>
+        of the tests.
+      </p>
+      <p v-else>
+        There is no known validation of this documentation with the libCellML
+        library.
+      </p>
+      <p v-if="neverTrue"></p>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
 
+const validationProvenance = ref(defaultValidationProvenance())
+
+const framework_url = computed(() => {
+  return `https://github.com/${validationProvenance.value.testing_framework.project}/tree/${validationProvenance.value.testing_framework.reference}`
+})
+const database_url = computed(() => {
+  return `https://github.com/${validationProvenance.value.testing_database.project}/tree/${validationProvenance.value.testing_database.reference}`
+})
+const framework_ref = computed(() => {
+  return validationProvenance.value.testing_framework.reference
+})
+const database_ref = computed(() => {
+  return validationProvenance.value.testing_database.reference
+})
+
+const neverTrue = computed(() => {
+  const url = `/generated/${store.state.current_documentation_version}/user/validation_provenance.json`
+  fetch(url).then((response) => {
+    response.json().then(
+      (content) => {
+        validationProvenance.value = content
+      },
+      () => {
+        validationProvenance.value = defaultValidationProvenance()
+      }
+    )
+  })
+  return false
+})
+
+const validationProvenanceIsKnown = computed(() => {
+  return (
+    validationProvenance.value.testing_framework.reference !== 'unknown' &&
+    validationProvenance.value.testing_database.reference !== 'unknown'
+  )
+})
+
 function getToPath(subpath) {
   return `/documentation/${store.state.current_documentation_version}/user/${subpath}/index`
+}
+
+function defaultValidationProvenance() {
+  return {
+    testing_framework: { reference: 'unknown', project: 'unknown' },
+    testing_database: { reference: 'unknown', project: 'unknown' },
+  }
 }
 </script>
 
