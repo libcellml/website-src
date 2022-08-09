810e79a740c9496c5c02387fc6c62ab6503d5af2
libcellml <libcellml@bn438065.uoa.auckland.ac.nz>
2022-08-09 12:40:51 +1200

    Merging changes into main from release_staging_v0.3.99-rc.1.

--- src/js/versions.js
+++ src/js/versions.js
@@ -1,5 +1,5 @@
 // This file is generated do not edit!
 // Changes made here will be overwritten when a release is made.
 export const getDocumentationVersions = () => {
-  return ['v0.3.2-dev.1', 'v0.2.0', 'v0.1.1']
+  return ['v0.3.99-rc.1', 'v0.2.0', 'v0.1.1']
 }
