d673aca2091e6d8fabcf2d2d70362cefed52b924
abi-git-user <abi.git.user@gmail.com>
2022-09-16 22:27:19 +1200

    Merging changes into main from release_staging_v0.3.104.

--- src/js/versions.js
+++ src/js/versions.js
@@ -1,5 +1,5 @@
 // This file is generated do not edit!
 // Changes made here will be overwritten when a release is made.
 export const getDocumentationVersions = () => {
-  return ['v0.3.103', 'v0.2.0', 'v0.1.1']
+  return ['v0.3.104', 'v0.2.0', 'v0.1.1']
 }
