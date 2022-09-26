65c62b493e793cf41f3faf84df86b908720a71e2
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-09-26 16:50:20 +0100

    List previous version assets.

--- src/components/download/DownloadBlurb.vue
+++ src/components/download/DownloadBlurb.vue
@@ -2,22 +2,22 @@
   <div id="downloadBlurb">
     <v-row>
       <v-col>
-        <p>
+        <p class="mb-4">
           libCellML is intended to be lightweight and transportable and as such
           the installers should contain everything you need to use libCellML. If
           you are building against the libCellML library,
           <strong>for Linux only</strong>, you will need to install the
           additional development package.
         </p>
-        <p>
+        <p class="mb-4">
           When using Python the recommended installation method is to install
           the <strong>libcellml</strong> package with pip.
         </p>
-        <p>
+        <p class="mb-4">
           When using the Javascript the recommended installation method is to
           install the <strong>libcellml.js</strong> package with yarn.
         </p>
-        <p>
+        <p class="mb-4">
           A complete list of historical versions for each operating system is
           shown below.
         </p>
