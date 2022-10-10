9ec2d5f5f8c33ceb4b2936a7a77d8fddf584120e
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-10-10 17:41:35 +0200

    Tweaks to sphinx.css.

--- src/views/DocumentationUser.vue
+++ src/views/DocumentationUser.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="user">
-    <sphinx-page :baseURL="`/generated/${store.state.current_documentation_version}/user`" pageNotFoundName="NotFound" />
+    <sphinx-page
+      :baseURL="`/generated/${store.state.current_documentation_version}/user`"
+      pageNotFoundName="NotFound"
+    />
   </div>
 </template>
 
 <script setup>
-import '../css/sphinx.css'
-
 import { useStore } from 'vuex'
 
 import { SphinxPage } from 'vue3-sphinx-xml'
 
 const store = useStore()
-
 </script>
+
+<style src="../css/sphinx.css"></style>
