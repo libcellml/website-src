f62937cc6f22332f5813b3cb46a9dc636ad53669
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-10-11 16:00:53 +1300

    Make the documentation handling more flexible to match the documentation compiled through the release process.

---
+++ src/views/SphinxDocsSansVersion.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="route.meta.subDoc">
+    <sphinx-page
+      :baseURL="`/generated/${route.meta.subDoc}`"
+      pageNotFoundName="NotFound"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import { SphinxPage } from 'vue3-sphinx-xml'
+
+const route = useRoute()
+console.log('000000000')
+console.log(route.name)
+console.log(route)
+</script>
